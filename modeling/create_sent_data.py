729c5057af3c788f93e0c936d799b2b88487ae1e
logan duffy <bsbell21@gmail.com>
2016-09-08 13:19:19 -0700

    building sent data scipt

--- modeling/create_sent_data.py
+++ modeling/create_sent_data.py
@@ -5,7 +5,17 @@ from random import randint
 if __name__ == '__main__':
 	fname = sys.argv[1]
 	df = pd.read_json(fname)
+	df = df.set_index('review_id')
+	d = df.to_dict()
+	d_new = {'review_id': [], 'sentence': [], 'sentiment': []}
 	sent = ['Positive', 'Neutral', 'Negative']
-	df['sentiment'] = df['business_id'].apply(lambda x: sent[randint(0,2)])
-	df.to_json('Training' + fname)
+	for k, v in d:
+		sentences = v['text'].split('.')
+		for sentence in sentences:
+			d_new['review_id'].append(k)
+			d_new['sentence'].append(sentence)
+			d_new['sentiment'].append(sent[randint(0,2)])
+
+	df_sent = pd.DataFrame(d_new).T
+	df_sent.to_json('Training' + fname)
 
