d1ab157baa29dca87331206a35e9628c6b090e57
logan duffy <bsbell21@gmail.com>
2016-09-08 12:22:20 -0700

    adding convert

---
+++ modeling/converttocsv.py
@@ -0,0 +1,32 @@
+'''
+Convert Yelp Academic Dataset from JSON to CSV
+
+Requires Pandas (https://pypi.python.org/pypi/pandas)
+
+By Paul Butler, No Rights Reserved
+'''
+
+import json
+import pandas as pd
+from glob import glob
+import sys
+
+def convert(x):
+    ''' Convert a json string to a flat python dictionary
+    which can be passed into Pandas. '''
+    ob = json.loads(x)
+    for k, v in ob.items():
+        if isinstance(v, list):
+            ob[k] = ','.join(v)
+        elif isinstance(v, dict):
+            for kk, vv in v.items():
+                ob['%s_%s' % (k, kk)] = vv
+            del ob[k]
+    return ob
+
+if __name__ in '__main__':
+    json_filename = sys[1]
+    csv_filename = '%s.csv' % json_filename[:-5]
+    print 'Converting %s to %s' % (json_filename, csv_filename)
+    df = pd.DataFrame([convert(line) for line in file(json_filename)])
+    df.to_csv(csv_filename, encoding='utf-8', index=False)
